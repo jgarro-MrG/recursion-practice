ccf32846e5b3e85337af0818fde3ee2d61a0bd5f
Jorge Garro <jorge.garroelizondo@jpschools.org>
2025-04-08 14:17:47 +0000

    factorial done

--- src/recursion.js
+++ src/recursion.js
@@ -1,3 +1,5 @@
+
+
 // Solve all of the following prompts using recursion.
 
 // 1. Calculate the factorial of a number.  The factorial of a non-negative integer n,
@@ -5,7 +7,13 @@
 // Example:  5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5);  // 120
 var factorial = function(n) {
- 
+  if (n > 1) {
+    return n * factorial (n - 1);
+  } else if (n >= 0 ){
+    return 1; 
+  } else {
+    return null;
+  }
 };
 
 // 2. Compute the sum of an array of integers.
