4b02ccc6860c67762e7296969a7338c873ffdbaa
JG <coding@ndsworks.com>
2025-04-08 17:19:43 -0500

    sumBelow done

--- src/recursion.js
+++ src/recursion.js
@@ -51,8 +51,20 @@ var isEven = function(n) {
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
 // sumBelow(7); // 21
-var sumBelow = function(n) {
+var sumBelow = function(n) {  
+  if (n > 0) {
+    if (n - 1 <= 0 ) {
+      return 0
+    } 
+    return n - 1  + sumBelow(n - 1);  
+  } else {    
+    if (n + 1 >= 0 ) {
+      return 0
+    } 
+    return n + 1  + sumBelow(n + 1); 
+  }
 };
+
 
 // 6. Get the integers in range (x, y).
 // Example:  range(2, 9);  // [3, 4, 5, 6, 7, 8]
